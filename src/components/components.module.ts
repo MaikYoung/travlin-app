d9c5517dff843b60be8f2fe5c1c8ce4168c1f327
Mike Young <miguel@kasfactory.net>
2018-07-01 21:12:10 +0200

    added components and translation json

---
+++ src/components/components.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular/module';
+@NgModule({
+	declarations: [
+	],
+	imports: [
+	],
+	exports: [
+	]
+})
+export class ComponentsModule {}
